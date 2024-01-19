f40dc60dc8914b8c1f889d06e79a1a7c789d6560
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-01-19 15:54:08 +0530

    Add routing functionality to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { About, Footer, Header, Landing } from "./Router";
 
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-        WhiteCoatLink
-      </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} exact />
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Router.js
@@ -0,0 +1,6 @@
+import Landing from "./pages/Landing";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+
+export {Landing, Header, Footer, About};
